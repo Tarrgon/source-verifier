a518ad4b52a8378c448ca4f0f83d1f1b6a81fd9d
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-05-27 12:06:08 -0400

    Type aliases

--- server/src/server/types.d.ts
+++ server/src/server/types.d.ts
@@ -1,7 +1,9 @@
 import type { Result, ResultExtras } from "../checkers";
 import type { BaseSourceData, DatabasePost, Rename, SourceDataMap } from "../modules";
 
-type Response = Partial<Omit<BaseSourceData, '_id'> & Rename<Omit<DatabasePost, 'sources'>, '_id', 'id'> & Result> & { id: number }
+type BaseSourceDataWithoutId = Omit<BaseSourceData, '_id'>;
+type DatabasePostWithoutSources = Omit<DatabasePost, 'sources'>;
+type Response = Partial<BaseSourceDataWithoutId & Rename<DatabasePostWithoutSources, '_id', 'id'> & ResultExtras> & { id: number }
 
 export type ServerResponse = CompleteResponse | IncompleteResponse;
 export type CompleteResponse = Required<Omit<Response, 'notIndexed' | 'queued' | 'notPending' | 'unsupported'>>;
