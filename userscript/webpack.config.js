eee1a79860393deddd2382647e5993a90b38e270
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-05-26 22:18:22 -0400

    Userscript initialization

---
+++ userscript/webpack.config.js
@@ -0,0 +1,13 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./tmp-dist/index.js",
+  output: {
+    filename: "source-verifier-userscript.user.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
