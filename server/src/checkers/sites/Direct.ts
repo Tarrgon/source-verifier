0a85a362eaa9890ca463f00bbea9a0ae7d3d7e15
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-05-28 14:22:50 -0400

    Piczel direct

--- server/src/checkers/sites/Direct.ts
+++ server/src/checkers/sites/Direct.ts
@@ -37,7 +37,6 @@ export default class DirectSourceChecker extends SourceChecker {
       /^https?:\/\/files\.mastodon\.social\/media_attachments\/files\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
       /^https?:\/\/cdn\.discordapp\.com\/attachments\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
       /^https?:\/\/staging\.cohostcdn\.org\/attachment\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
-      // TODO: Support non-direct
       /^https?:\/\/cdn\.buymeacoffee\.com\/uploads\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
       /^https?:\/\/.*\.rule34\.xxx\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
       /^https?:\/\/r34i\.paheal-cdn\.net\/.*/,
@@ -54,7 +53,8 @@ export default class DirectSourceChecker extends SourceChecker {
       /^https?:\/\/.*\.pillowfort\.social\/posts\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
       /^https?:\/\/images\.plurk\.com\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
       /^https?:\/\/downloads\.fanbox\.cc\/images\/post\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
-      /^https?:\/\/co\.llection\.pics\/_images\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/
+      /^https?:\/\/co\.llection\.pics\/_images\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
+      /^https?:\/\/piczel\.tv\/static\/uploads\/gallery_image\/.*\.(png|jpg|jpeg|webp|gif|webm|mp4).*/,
     ];
 
     this.previewRegex = [
