5aab542467bea58c599e3e6a39336fae8ec9fee5
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-05-31 17:57:10 -0400

    Add cors headers

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 import type { Express as ExpressServer } from 'express-serve-static-core';
 import fs from 'fs';
 import path from 'path';
@@ -9,6 +10,7 @@ export default async function (): Promise<ExpressServer> {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const app = express();
 
+  app.use(cors());
   app.use(bodyParser.json({ limit: '1KB' }));
 
   // routers
