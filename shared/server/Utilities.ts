7c465ceffb1d6712edb3738b1579860f4e6434e8
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-06-01 16:45:36 -0400

    Add normalizeURL to shared

--- shared/server/Utilities.ts
+++ shared/server/Utilities.ts
@@ -7,3 +7,31 @@ export function isCompleteResponse(response: ServerResponse): response is Comple
 
   return true;
 }
+
+export async function normalizeURL(url: URL | string, getBlueskyDid: (handle: string) => Promise<string>): Promise<string> {
+  if (url == '') return '';
+  if (!(url instanceof URL)) url = new URL(url.startsWith('-') ? url.slice(1) : url);
+
+  if (url.hostname == 'twitter.com') url.hostname = 'x.com';
+  else if (url.hostname.endsWith('weasyl.com')) {
+    if (!url.pathname.match(/\d+$/)) {
+      const id = /\/submissions?\/(\d+)/.exec(url.pathname)![1];
+      url = new URL(`https://www.weasyl.com/submission/${id}`);
+    }
+  }
+
+  let regexData: RegExpExecArray | null = null;
+  if ((regexData = /https:\/\/bsky\.app\/profile\/(.*)\/post/.exec(url.toString())) != null) {
+    if (!regexData[1].startsWith('did:plc:')) {
+      const did = await getBlueskyDid(regexData[1]);
+
+      if (did) {
+        const u = url.toString().replace(regexData[1], did);
+        url = new URL(u);
+      }
+    }
+  }
+
+  const u = url.toString();
+  return u.endsWith('/') ? u.slice(0, -1) : u;
+}
