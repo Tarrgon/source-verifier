689cad182ef563eeb50e4da9524101eb237af247
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-06-17 13:02:03 -0400

    Fix crash

--- shared/server/Utilities.ts
+++ shared/server/Utilities.ts
@@ -8,31 +8,36 @@ export function isCompleteResponse(response: ServerResponse): response is Comple
   return true;
 }
 
-export async function normalizeURL(url: URL | string, getBlueskyDid: (handle: string) => Promise<string | null>): Promise<string> {
-  if (url == '') return '';
-  if (!(url instanceof URL)) url = new URL(url.startsWith('-') ? url.slice(1) : url);
+export async function normalizeURL(url: URL | string, getBlueskyDid: (handle: string) => Promise<string | null>): Promise<string | null> {
+  try {
+    if (url == '') return '';
+    if (!(url instanceof URL)) url = new URL(url.startsWith('-') ? url.slice(1) : url);
 
-  let regexData: RegExpExecArray | null = null;
+    let regexData: RegExpExecArray | null = null;
 
-  if (url.hostname == 'twitter.com') url.hostname = 'x.com';
-  else if (url.hostname.endsWith('weasyl.com')) {
-    if (!url.pathname.match(/\d+$/)) {
-      const id = /\/submissions?\/(\d+)/.exec(url.pathname)![1];
-      url = new URL(`https://weasyl.com/submission/${id}`);
-    }
-  } else if ((regexData = /https:\/\/bsky\.app\/profile\/(.*)\/post/.exec(url.toString())) != null) {
-    if (!regexData[1].startsWith('did:plc:')) {
-      const did = await getBlueskyDid(regexData[1]);
+    if (url.hostname == 'twitter.com') url.hostname = 'x.com';
+    else if (url.hostname.endsWith('weasyl.com')) {
+      if (!url.pathname.match(/\d+$/)) {
+        const id = /\/submissions?\/(\d+)/.exec(url.pathname)![1];
+        url = new URL(`https://weasyl.com/submission/${id}`);
+      }
+    } else if ((regexData = /https:\/\/bsky\.app\/profile\/(.*)\/post/.exec(url.toString())) != null) {
+      if (!regexData[1].startsWith('did:plc:')) {
+        const did = await getBlueskyDid(regexData[1]);
 
-      if (did) {
-        const u = url.toString().replace(regexData[1], did);
-        url = new URL(u);
+        if (did) {
+          const u = url.toString().replace(regexData[1], did);
+          url = new URL(u);
+        }
       }
     }
-  }
 
-  let u = url.toString();
-  u = u.replace('://www.', '://');
-  u = u.endsWith('/') ? u.slice(0, -1) : u;
-  return u;
+    let u = url.toString();
+    u = u.replace('://www.', '://');
+    u = u.endsWith('/') ? u.slice(0, -1) : u;
+    return u;
+  } catch(e) {
+    console.error(`Error normalizing url: "${url}"`);
+    return null;
+  }
 }
