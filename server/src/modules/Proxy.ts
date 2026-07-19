7a71e1efd75380bc40b55399da00b4457858ed9d
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-07-19 16:10:57 -0400

    Whoops...

--- server/src/modules/Proxy.ts
+++ server/src/modules/Proxy.ts
@@ -3,7 +3,7 @@ import https from 'https';
 import { config } from '../config';
 import type Stream from 'stream';
 
-const PROXY_AUTHENTICATION = 'Basic ' + Buffer.from(config.PROXY_USERNAME + ':' + config.PROXY_USERNAME).toString('base64');
+const PROXY_AUTHENTICATION = 'Basic ' + Buffer.from(config.PROXY_USERNAME + ':' + config.PROXY_PASSWORD).toString('base64');
 const PROXY_IP = config.PROXY_URI!.split(':')[0];
 const PROXY_PORT = parseInt(config.PROXY_URI!.split(':')[1]);
 
@@ -19,6 +19,10 @@ function connectToProxy(hostname: string, secure: boolean = true): Promise<Strea
       },
     }).on('connect', (res, socket) => {
       if (res.statusCode == 200) return resolve(socket);
+      return resolve(null);
+    }).on('error', (e) => {
+      console.error(`Error fetching: ${hostname} (${secure}) with proxy:`);
+      console.error(e);
       return resolve(null);
     });
   });
@@ -36,37 +40,43 @@ export function fetchProxy(url: string, options?: { headers?: { [header: string]
 
     const _http = secure ? https : http;
 
-    _http.get({
-      host: _url.hostname,
-      path: _url.pathname,
-      headers: options?.headers ? options.headers : undefined,
-      agent
-    }, (res) => {
-      if (!res.statusCode || res.statusCode < 200 || res.statusCode >= 300) return reject(`Fetching on proxy had non-200 status code: ${res.statusCode}`);
+    try {
+      _http.get({
+        host: _url.hostname,
+        path: _url.pathname,
+        headers: options?.headers ? options.headers : undefined,
+        agent
+      }, (res) => {
+        if (!res.statusCode || res.statusCode < 200 || res.statusCode >= 300) return reject(`Fetching on proxy had non-200 status code: ${res.statusCode}`);
 
-      const body: Buffer[] = [];
+        const body: Buffer[] = [];
 
-      res.on('data', (d) => {
-        body.push(d);
-      });
-
-      res.on('end', () => {
-        // @ts-ignore Typescript unfortunately doesn't have any idea how this works...
-        const headers = res.rawHeaders.reduce((acc, cur, index, arr) => {
-          if (index % 2 == 0) {
-            return [...acc, [arr[index].toLowerCase(), arr[index + 1]]];
-          }
-
-          return acc;
-        }, []) as [string, string][];
-
-        const result = new Response(new Uint8Array(Buffer.concat(body).buffer), {
-          status: res.statusCode,
-          headers: new Headers(headers)
+        res.on('data', (d) => {
+          body.push(d);
         });
 
-        return resolve(result);
+        res.on('end', () => {
+          // @ts-ignore Typescript unfortunately doesn't have any idea how this works...
+          const headers = res.rawHeaders.reduce((acc, cur, index, arr) => {
+            if (index % 2 == 0) {
+              return [...acc, [arr[index].toLowerCase(), arr[index + 1]]];
+            }
+
+            return acc;
+          }, []) as [string, string][];
+
+          const result = new Response(new Uint8Array(Buffer.concat(body).buffer), {
+            status: res.statusCode,
+            headers: new Headers(headers)
+          });
+
+          return resolve(result);
+        });
       });
-    });
+    } catch (e) {
+      console.error(`Error fetching: ${url} with proxy:`);
+      console.error(e);
+      return reject('Error fetching with proxy...');
+    }
   });
 }
