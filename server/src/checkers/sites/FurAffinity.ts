4fd47dea10a4e2774234716e0c367390eeb5f979
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-07-19 16:18:10 -0400

    Disable FA again

--- server/src/checkers/sites/FurAffinity.ts
+++ server/src/checkers/sites/FurAffinity.ts
@@ -11,6 +11,8 @@ export default class FurAffinitySourceChecker extends SourceChecker {
     this.supported = [
       /^https?:\/\/.*furaffinity\.net\/(view|full)\/(\d+).*/
     ];
+
+    this.enabled = false;
   }
 
   async _internalProcessPost(post: SourceCheckQueueItem, source: string): Promise<SourceData> {
