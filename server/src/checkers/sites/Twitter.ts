eaa47f9742a4677d68c395c759c6a294816d0aca
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-07-27 14:46:24 -0400

    Fix twitter authors

--- server/src/checkers/sites/Twitter.ts
+++ server/src/checkers/sites/Twitter.ts
@@ -73,7 +73,7 @@ export default class TwitterSourceChecker extends SourceChecker {
         urls = urls.filter(s => !(!s.url.includes('name=orig') && s.url.includes('format=png')));
 
         for (const urlData of urls) {
-          const data = await SourceChecker.processDirectLink(post, urlData.url, urlData.isPreview, authorName) as ScoredSourceData;
+          const data = await SourceChecker.processDirectLink(post, urlData.url, urlData.isPreview, authorName ? [authorName] : []) as ScoredSourceData;
 
           if (!data || data.error || data.unknown || data.unsupported) {
             continue;
